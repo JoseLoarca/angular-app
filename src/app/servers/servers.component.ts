41b699926e6f8f791fba2b079990a2aa86f10288
Jose Loarca <joseloarca97@icloud.com>
2018-03-22 16:34:56 -0600

    testing data binding and string interpolation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverName = 'BIG RED FOX';
+  username = '';
+  serverCreationStatus = 'No server was created!';
+  allowClickButton = false;
 
-  constructor() { }
+  constructor() {
+    // () => {} es lo mismo que function(){}
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! The name is: ' + this.serverName;
+  }
+
+  resetUsername() {
+    this.username = '';
+    this.allowClickButton = false;
+  }
+
+  verifyButton() {
+    this.allowClickButton = (this.username !== '');
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
