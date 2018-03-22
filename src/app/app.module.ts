657bc2009372254d9bfc1ce8ce939d78ee0068be
Jose Loarca <joseloarca97@icloud.com>
2018-03-22 15:27:41 -0600

    testing angular components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
